880ee6acda425507a63383ce3a39351facc106f3
jacdylngab <160424490+jacdylngab@users.noreply.github.com>
2024-02-25 20:18:52 -0600

    Add files via upload

---
+++ static/chatbot.js
@@ -0,0 +1,5 @@
+function chatOpen() {
+  document.getElementById("chat-open").style.display = "none";
+  document.getElementById("chat-close").style.display = "block";
+  document.getElementById("chat-window1").style.display = "block";
+} 
